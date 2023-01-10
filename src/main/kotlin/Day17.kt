41d777a4e268d3780f64a851f6ccbc4ce7d2dba7
Julian Baumgartner <julian.baumgaertner@bmw.de>
2023-01-10 21:18:03 +0100

    Day17

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -1,8 +1,7 @@
 import utils.*
-import kotlin.math.abs
 
 fun main() {
-    Day17(IO.TYPE.SAMPLE).test(45)
+    Day17(IO.TYPE.SAMPLE).test(45, 112)
     Day17().solve()
 }
 
@@ -12,22 +11,15 @@ class Day17(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("Trick Shot", inputType = 
     private val target = cornerPoints.toTarget()
 
     override fun part1(): Int {
-        val maxX = cornerPoints[1]
-        val maxY = cornerPoints[2]
-        val speeds = (1..cornerPoints[1]).flatMap { x ->
-            (1..abs(maxY)).map { y ->
-                Position(x, y)
-            }
-        }
+        val speeds = generateSpeeds()
+        val paths = speeds.mapNotNull { speed -> speed.pathToTarget() }
+        return paths.maxOf { trajectory -> trajectory.maxOf { it.y } }
+    }
 
-        return speeds.maxOf { speed ->
-            val path = Position.origin.generateTrajectory(speed).takeWhile { 
-                    (position, _) -> position.x < maxX && position.y > maxY
-            }
-            
-            val last = path.last().let { it.first + it.second }
-            if (last in target) path.maxOf { it.first.y } else 0
-        }
+
+    override fun part2(): Int {
+        val speeds = generateSpeeds()
+        return speeds.count { speed -> speed.pathToTarget() != null }
     }
 
     private fun Position.generateTrajectory(initialSpeed: Position) = generateSequence(this to initialSpeed) {
@@ -43,8 +35,22 @@ class Day17(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("Trick Shot", inputType = 
         )
     }
 
-    override fun part2(): Any? {
-        return "not yet implement"
+    private fun generateSpeeds() = (0..cornerPoints[1]).flatMap { x ->
+        (cornerPoints[2]..128).map { y ->
+            Position(x, y)
+        }
+    }
+
+    private fun Position.pathToTarget(): List<Position>? { 
+        val path = Position.origin.generateTrajectory(this)
+            .takeWhile { (position, _) ->
+                position.x <= cornerPoints[1] && position.y >= cornerPoints[2]
+            }.toList()
+
+        val last = path.last()
+
+        return if (last.first !in target && (last.first + last.second) !in target) null
+        else path.map { it.first }
     }
 
     private fun String.parseCornerPoints() = """-?\d+""".toRegex().findAll(this).take(4).map { it.value.toInt() }.toList()
@@ -57,4 +63,3 @@ class Day17(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("Trick Shot", inputType = 
         }
     }
 }
-           
