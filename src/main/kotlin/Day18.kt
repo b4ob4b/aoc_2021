ee085b4e977a99b2e5b1feb6215ed34f634efe6c
Julian Baumgartner <julian.baumgaertner@bmw.de>
2023-01-12 21:42:50 +0100

    Day18 part2

--- src/main/kotlin/Day18.kt
+++ src/main/kotlin/Day18.kt
@@ -4,38 +4,35 @@ import utils.print
 import utils.splitLines
 
 fun main() {
-    Day18(IO.TYPE.SAMPLE).test(4140)
+    Day18(IO.TYPE.SAMPLE).test(4140, 3993)
     Day18().solve()
 }
 
 class Day18(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType) {
 
-    override fun part1(): Int {
-        val numbers = input.splitLines()
+    private val numbers = input.splitLines()
+    private val snailfishNumbers = numbers.map { SnailFishNumber.from(it) }
 
-        val snailfishNumbers = numbers.map { SnailFishNumber.from(it) }
+    override fun part1(): Int {
         return snailfishNumbers.reduce { acc, number -> (acc + number).reduce() }.calculateMagnitude()
     }
 
     override fun part2(): Any? {
-        return "not yet implement"
+        return snailfishNumbers.flatMap { i ->
+            snailfishNumbers.mapNotNull { j ->
+                if (i != j) i + j else null
+            }.map { it.reduce().calculateMagnitude() }
+        }.max()
     }
 
     sealed class SnailFishNumber {
-        val depth: Int
-            get() {
-                return calculateDepth()
-            }
-
         abstract fun calculateMagnitude(): Int
-
-        abstract fun calculateDepth(): Int
 
         fun reduce(): SnailFishNumber {
             var number = this
             var explosion = number.explode()
             var split = explosion.split()
-            
+
             while (split != number) {
                 number = split
                 explosion = number.explode()
@@ -57,7 +54,7 @@ class Day18(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType)
         abstract fun explode(depth: Int, found: Boolean = false): Explosion
 
         operator fun plus(other: SnailFishNumber): SnailFishNumber {
-            return Number(this to other)
+            return Number(this, other)
         }
 
         abstract fun split(): SnailFishNumber
@@ -75,54 +72,50 @@ class Day18(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType)
 
     data class Explosion(val left: Int? = null, val snailFishNumber: SnailFishNumber, val right: Int? = null, val found: Boolean = false)
 
-    data class Number(val pair: Pair<SnailFishNumber, SnailFishNumber>) : SnailFishNumber() {
+    data class Number(val left: SnailFishNumber, val right: SnailFishNumber) : SnailFishNumber() {
         companion object {
             fun from(string: String): Number {
                 var current = ""
                 var brackets = 0
-                val pair = buildList {
-                    for (char in string.drop(1).dropLast(1)) {
-                        if (char == '[') brackets++
-                        if (char == ']') brackets--
-                        if (char == ',' && brackets == 0) {
-                            add(SnailFishNumber.from(current))
-                            current = ""
-                            continue
-                        }
-                        current += char
+                var left: SnailFishNumber? = null
+                for (char in string.drop(1).dropLast(1)) {
+                    if (char == '[') brackets++
+                    if (char == ']') brackets--
+                    if (char == ',' && brackets == 0) {
+                        left = SnailFishNumber.from(current)
+                        current = ""
+                        continue
                     }
-                    add(SnailFishNumber.from(current))
-                }.zipWithNext().single()
+                    current += char
+                }
+                val right = SnailFishNumber.from(current)
 
-                return Number(pair)
+                if (left == null) throw Exception("unexpected input")
+                return Number(left, right)
             }
         }
 
         override fun calculateMagnitude(): Int {
-            val (left, right) = pair
             return left.calculateMagnitude() * 3 + right.calculateMagnitude() * 2
         }
 
-        override fun calculateDepth(): Int {
-            return pair.toList().maxOf { it.depth } + 1
-        }
-
         override fun split(): SnailFishNumber {
-            var (left, right) = pair
+            var right = right
             val splitLeft = left.split()
             if (splitLeft == left) {
                 right = right.split()
             }
-            return Number(splitLeft to right)
+            return Number(splitLeft, right)
         }
 
+
         override fun explode(depth: Int, found: Boolean): Explosion {
-            var (left, right) = pair
+            var left = left
+            var right = right
             var explosionFound = found
             if (depth == 4) {
                 return Explosion((left as Value).value, Value(0), (right as Value).value, true)
             }
-
             var leftUp: Int? = null
             var rightUp: Int? = null
 
@@ -155,29 +148,24 @@ class Day18(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType)
                 }
             }
 
-
-
-            return Explosion(leftUp, Number(left to right), rightUp, explosionFound)
+            return Explosion(leftUp, Number(left, right), rightUp, explosionFound)
         }
 
         private fun addRight(newLeft: Int): SnailFishNumber {
-            val (left, right) = pair
             return when (right) {
-                is Value -> Number(left to Value(right.value + newLeft))
-                is Number -> Number(left to right.addRight(newLeft))
+                is Value -> Number(left, Value(right.value + newLeft))
+                is Number -> Number(left, right.addRight(newLeft))
             }
         }
 
         private fun addLeft(newRight: Int): SnailFishNumber {
-            val (left, right) = pair
             return when (left) {
-                is Value -> Number(Value(left.value + newRight) to right)
-                is Number -> Number(left.addLeft(newRight) to right)
+                is Value -> Number(Value(left.value + newRight), right)
+                is Number -> Number(left.addLeft(newRight), right)
             }
         }
 
         override fun toString(): String {
-            val (left, right) = pair
             return "[$left,$right]"
         }
     }
@@ -187,18 +175,17 @@ class Day18(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType)
             return value
         }
 
-        override fun calculateDepth() = 0
         override fun explode(depth: Int, found: Boolean): Explosion {
-            return Explosion(snailFishNumber = this)
+            return Explosion(value, Value(0), value, true)
         }
 
         override fun split(): SnailFishNumber {
+            val half = value / 2
+            val isEven = value % 2 == 0
             return if (value > 9) {
-                if (value % 2 == 0) {
-                    Number(Value(value / 2) to Value(value / 2))
-                } else {
-                    Number(Value(value / 2) to Value(value / 2 + 1))
-                }
+                val left = Value(half)
+                val right = Value(if (isEven) half else half + 1)
+                Number(left, right)
             } else {
                 this
             }
