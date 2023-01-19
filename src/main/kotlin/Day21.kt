a5ae7a686a4ef21b94fc1186053ea7c662346096
Julian Baumgartner <julian.baumgaertner@bmw.de>
2023-01-19 21:30:06 +0100

    Day21

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -1,7 +1,7 @@
 import utils.*
 
 fun main() {
-    Day21(IO.TYPE.SAMPLE).test(739785)
+    Day21(IO.TYPE.SAMPLE).test(739785, 444356092776315L)
     Day21().solve()
 }
 
@@ -12,43 +12,62 @@ class Day21(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("Dirac Dice", inputType = 
         val next = it + 1
         if (next == 11) 1 else next
     }
+    private val player1 = Player(startingPosition.first, 0)
+    private val player2 = Player(startingPosition.second, 0)
     private val dice = Dice()
-    
+    private val memo: MutableMap<Pair<Player, Player>, Pair<Long, Long>> = mutableMapOf()
 
+    override fun part1(): Int {
+        val score = player1 playDiracDice player2
+        return score.toList().product()
+    }
 
-    override fun part1(): Any? {
-        var p1 = startingPosition.first
-        var p2 = startingPosition.second
+    override fun part2(): Long {
+        val wins = player1 playQuantumDice player2
+        return wins.toList().max()
+    }
 
-        var s1 = 0
-        var s2 = 0
+    private infix fun Player.playDiracDice(other: Player): Pair<Int, Int> {
+        val diceNumbers = dice.roll()
+        val newSpace = spaces.take(space + diceNumbers.sum()).last()
+        val newScore = score + newSpace
 
-        while (s2 < 1000) {
-            val d1 = dice.roll()
-            p1 = spaces.take(p1 + d1.sum()).last()
-            s1 += p1
-            "${dice.rolls}: Player 1 rolls $d1 and moves to space $p1 for a total score of $s1".print()
-            if (s1 >= 1000) break
+        if (newScore >= 1000) return other.score to dice.rolls
 
-            val d2 = dice.roll()
-            p2 = spaces.take(p2 + d2.sum()).last()
-            s2 += p2
-            "${dice.rolls}: Player 2 rolls $d2 and moves to space $p2 for a total score of $s2".print()
+        return other playDiracDice Player(newSpace, newScore)
+    }
+
+    private infix fun Player.playQuantumDice(other: Player): Pair<Long, Long> {
+        if (this.score >= 21) return 1L to 0L
+        if (other.score >= 21) return 0L to 1L
+
+        if ((this to other) in memo) return memo[this to other]!!
+
+        var wins1 = 0L
+        var wins2 = 0L
+
+        (1..3).forEach { d1 ->
+            (1..3).forEach { d2 ->
+                (1..3).forEach { d3 ->
+                    val newSpace = spaces.take(this.space + d1 + d2 + d3).last()
+                    val newScore = newSpace + this.score
+
+                    val (w2, w1) = other playQuantumDice Player(newSpace, newScore)
+                    wins1 += w1
+                    wins2 += w2
+                }
+            }
         }
-
-        val scoreLosing = if (s1 < s2) s1 else s2
-        return dice.rolls * scoreLosing
+        memo[this to other] = wins1 to wins2
+        return wins1 to wins2
     }
 
-    override fun part2(): Any? {
-        return "not yet implement"
-    }
+    data class Player(val space: Int, val score: Int)
 
     class Dice {
-
         var rolls = 0
         var current = 0
-        val maxFaces = 100
+        private val maxFaces = 100
 
         fun roll(): List<Int> {
             rolls += 3
@@ -60,6 +79,5 @@ class Day21(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("Dirac Dice", inputType = 
             current = faces.last()
             return faces
         }
-
     }
 }           
